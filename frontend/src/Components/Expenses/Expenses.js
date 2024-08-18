714c62b7aac0623580cdf78a3119b56f4de8b92e
msalaheen01 <mus2004@nyu.edu>
2024-08-18 13:47:18 -0400

    Update Expenses.js
    some color changes

--- frontend/src/Components/Expenses/Expenses.js
+++ frontend/src/Components/Expenses/Expenses.js
@@ -52,8 +52,8 @@ const ExpenseStyled = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
-        background: #FCF6F9;
-        border: 2px solid #FFFFFF;
+        background: lightblue;
+        border: 2px solid skyblue;
         box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
         border-radius: 20px;
         padding: 1rem;
